a79523b1c783b0ca7dd05f7fd2b7355d7f45783a
Seth Cattanach <scattana@nd.edu>
2020-02-24 14:20:08 -0500

    initial commit - libraries and early INO code

---
+++ droid-code-ps3/droid-code-ps3.ino
@@ -0,0 +1,570 @@
+// =======================================================================================
+//                     PS3 Test Sketch for Notre Dame Droid Class
+// =======================================================================================
+//                          Last Revised Date: 03/22/2018
+//                             Revised By: Prof McLaughlin
+// =======================================================================================
+// ---------------------------------------------------------------------------------------
+//                          Libraries
+// ---------------------------------------------------------------------------------------
+
+// PS3 libraries
+#include <PS3BT.h>
+#include <usbhub.h>
+
+// Servo motor library
+#include <Servo.h>
+
+// OLED display libraries
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Sound system library
+#include <MP3Trigger.h>
+
+
+
+// ---------------------------------------------------------------------------------------
+//                       Debug - Verbose Flags
+// ---------------------------------------------------------------------------------------
+#define SHADOW_DEBUG       //uncomment this for console DEBUG output
+
+
+// ----------------------------
+// Constants for OLED display
+// ----------------------------
+#define SCREEN_WIDTH 128   // OLED display, width (pixels)
+#define SCREEN_HEIGHT 64   // OLED display, height (pixels)
+
+
+// ---------------------------------------------------------------------------------------
+//                 Setup for USB, Bluetooth Dongle, & PS3 Controller
+// ---------------------------------------------------------------------------------------
+USB Usb;
+BTD Btd(&Usb);
+PS3BT *PS3Controller=new PS3BT(&Btd);
+
+// ---------------------------------------------------------------------------------------
+//                 Setup for Servo motor
+// ---------------------------------------------------------------------------------------
+Servo myServo;
+int pos = 90;
+
+// ---------------------------------------------------------------------------------------
+//                 Setup for MP3 Trigger/Sound System
+// ---------------------------------------------------------------------------------------
+MP3Trigger MP3Trigger;
+
+
+// ---------------------------------------------------------------------------------------
+//                 Setup for OLED display
+// ---------------------------------------------------------------------------------------
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// ---------------------------------------------------------------------------------------
+//    Output String for Serial Monitor Output
+// ---------------------------------------------------------------------------------------
+char output[300];
+
+// ---------------------------------------------------------------------------------------
+//    Deadzone range for joystick to be considered at nuetral
+// ---------------------------------------------------------------------------------------
+byte joystickDeadZoneRange = 15;
+
+// ---------------------------------------------------------------------------------------
+//    Used for PS3 Fault Detection
+// ---------------------------------------------------------------------------------------
+uint32_t msgLagTime = 0;
+uint32_t lastMsgTime = 0;
+uint32_t currentTime = 0;
+uint32_t lastLoopTime = 0;
+int badPS3Data = 0;
+
+boolean isPS3ControllerInitialized = false;
+boolean mainControllerConnected = false;
+boolean WaitingforReconnect = false;
+boolean isFootMotorStopped = true;
+
+// ---------------------------------------------------------------------------------------
+//    Used for PS3 Controller Click Management
+// ---------------------------------------------------------------------------------------
+long previousMillis = millis();
+boolean extraClicks = false;
+
+// =======================================================================================
+//                                 Main Program
+// =======================================================================================
+// =======================================================================================
+//                          Initialize - Setup Function
+// =======================================================================================
+
+
+
+void setup()
+{
+  
+    //Debug Serial for use with USB Debugging
+    Serial.begin(115200);
+    while (!Serial);
+    
+    if (Usb.Init() == -1)
+    {
+        Serial.print(F("\r\nOSC did not start"));
+        while (1); //halt
+    }
+
+    strcpy(output, "");
+    
+    Serial.print(F("\r\nBluetooth Library Started"));
+    
+    //Setup for PS3 Controller
+    PS3Controller->attachOnInit(onInitPS3Controller); // onInitPS3Controller is called upon a new connection
+
+    // SERVO setup
+    myServo.attach(9);
+
+    // SOUND SYSTEM setup
+    MP3Trigger.setup(&Serial2);
+    Serial2.begin(MP3Trigger::serialRate());
+}
+
+// =======================================================================================
+//           Main Program Loop - This is the recurring check loop for entire sketch
+// =======================================================================================
+void loop()
+{   
+
+    // update servo position
+    myServo.write(pos);
+
+    // trigger MP3 to update sound
+    MP3Trigger.update();
+
+    
+    
+    if ( !readUSB() )
+    {
+      //We have a fault condition that we want to ensure that we do NOT process any controller data!!!
+      printOutput(output);
+      return;
+    }
+    
+    checkController();
+  
+   if (extraClicks)
+   {
+      if ((previousMillis + 500) < millis())
+      {
+          extraClicks = false;
+      }
+   }
+  
+   printOutput(output);
+}
+
+// =======================================================================================
+//          Check Controller Function to show all PS3 Controller inputs are Working
+// =======================================================================================
+void checkController()
+{
+       if (PS3Controller->PS3Connected && PS3Controller->getButtonPress(UP) && !extraClicks)
+     {              
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: UP Selected.\r\n");
+            #endif
+            
+            previousMillis = millis();
+            extraClicks = true;
+            
+     }
+  
+     if (PS3Controller->PS3Connected && PS3Controller->getButtonPress(DOWN) && !extraClicks)
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: DOWN Selected.\r\n");
+            #endif                     
+            
+            previousMillis = millis();
+            extraClicks = true;
+       
+     }
+
+     if (PS3Controller->PS3Connected && PS3Controller->getButtonPress(LEFT) && !extraClicks)
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: LEFT Selected.\r\n");
+            #endif  
+            
+            previousMillis = millis();
+            extraClicks = true;
+
+     }
+     
+     if (PS3Controller->PS3Connected && PS3Controller->getButtonPress(RIGHT) && !extraClicks)
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: RIGHT Selected.\r\n");
+            #endif       
+            
+            previousMillis = millis();
+            extraClicks = true;
+                     
+     }
+     
+     if (PS3Controller->PS3Connected && PS3Controller->getButtonPress(CIRCLE) && !extraClicks)
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: CIRCLE Selected.\r\n");
+            #endif      
+            
+            previousMillis = millis();
+            extraClicks = true;
+
+            // servo
+            pos = 0;
+           
+     }
+
+     if (PS3Controller->PS3Connected && PS3Controller->getButtonPress(CROSS) && !extraClicks)
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: CROSS Selected.\r\n");
+            #endif       
+            
+            previousMillis = millis();
+            extraClicks = true;
+              
+     }
+     
+     if (PS3Controller->PS3Connected && PS3Controller->getButtonPress(TRIANGLE) && !extraClicks)
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: TRIANGLE Selected.\r\n");
+            #endif       
+            
+            previousMillis = millis();
+            extraClicks = true;
+
+            // play sound file
+            MP3Trigger.trigger(17);
+              
+     }
+     
+
+     if (PS3Controller->PS3Connected && PS3Controller->getButtonPress(SQUARE) && !extraClicks)
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: SQUARE Selected.\r\n");
+            #endif       
+            
+            previousMillis = millis();
+            extraClicks = true;
+
+            // servo
+            pos = 180;
+     }
+     
+     if (PS3Controller->PS3Connected && !extraClicks && PS3Controller->getButtonPress(L1))
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: LEFT 1 Selected.\r\n");
+            #endif       
+            
+            previousMillis = millis();
+            extraClicks = true;
+     }
+
+     if (PS3Controller->PS3Connected && !extraClicks && PS3Controller->getButtonPress(L2))
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: LEFT 2 Selected.\r\n");
+            #endif       
+            
+            previousMillis = millis();
+            extraClicks = true;
+     }
+
+     if (PS3Controller->PS3Connected && !extraClicks && PS3Controller->getButtonPress(R1))
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: RIGHT 1 Selected.\r\n");
+            #endif       
+            
+            previousMillis = millis();
+            extraClicks = true;
+     }
+
+     if (PS3Controller->PS3Connected && !extraClicks && PS3Controller->getButtonPress(R2))
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: RIGHT 2 Selected.\r\n");
+            #endif       
+            
+            previousMillis = millis();
+            extraClicks = true;
+     }
+
+     if (PS3Controller->PS3Connected && !extraClicks && PS3Controller->getButtonPress(SELECT))
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: SELECT Selected.\r\n");
+            #endif       
+            
+            previousMillis = millis();
+            extraClicks = true;
+     }
+
+     if (PS3Controller->PS3Connected && !extraClicks && PS3Controller->getButtonPress(START))
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: START Selected.\r\n");
+            #endif       
+            
+            previousMillis = millis();
+            extraClicks = true;
+     }
+
+     if (PS3Controller->PS3Connected && !extraClicks && PS3Controller->getButtonPress(PS))
+     {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Button: PS Selected.\r\n");
+            #endif       
+            
+            previousMillis = millis();
+            extraClicks = true;
+     }
+
+     if (PS3Controller->PS3Connected && ((abs(PS3Controller->getAnalogHat(LeftHatY)-128) > joystickDeadZoneRange) || (abs(PS3Controller->getAnalogHat(LeftHatX)-128) > joystickDeadZoneRange)))
+     {
+            
+            int currentValueY = PS3Controller->getAnalogHat(LeftHatY) - 128;
+            int currentValueX = PS3Controller->getAnalogHat(LeftHatX) - 128;
+            
+            char yString[5];
+            itoa(currentValueY, yString, 10);
+
+            char xString[5];
+            itoa(currentValueX, xString, 10);
+
+            #ifdef SHADOW_DEBUG
+                strcat(output, "LEFT Joystick Y Value: ");
+                strcat(output, yString);
+                strcat(output, "\r\n");
+                strcat(output, "LEFT Joystick X Value: ");
+                strcat(output, xString);
+                strcat(output, "\r\n");
+            #endif
+
+            char vol[5];
+            MP3Trigger.setVolume(map(currentValueY, 127, -128, 0, 255));
+            strcat(output, itoa(map(currentValueY, 127, -128, 0, 255), vol, 10));
+     }
+
+     if (PS3Controller->PS3Connected && ((abs(PS3Controller->getAnalogHat(RightHatY)-128) > joystickDeadZoneRange) || (abs(PS3Controller->getAnalogHat(RightHatX)-128) > joystickDeadZoneRange)))
+     {
+            int currentValueY = PS3Controller->getAnalogHat(RightHatY) - 128;
+            int currentValueX = PS3Controller->getAnalogHat(RightHatX) - 128;
+
+            char yString[5];
+            itoa(currentValueY, yString, 10);
+
+            char xString[5];
+            itoa(currentValueX, xString, 10);
+
+            #ifdef SHADOW_DEBUG
+                strcat(output, "RIGHT Joystick Y Value: ");
+                strcat(output, yString);
+                strcat(output, "\r\n");
+                strcat(output, "RIGHT Joystick X Value: ");
+                strcat(output, xString);
+                strcat(output, "\r\n");
+            #endif       
+     }
+}
+
+// =======================================================================================
+//           PPS3 Controller Device Mgt Functions
+// =======================================================================================
+// =======================================================================================
+//           Initialize the PS3 Controller Trying to Connect
+// =======================================================================================
+void onInitPS3Controller()
+{
+    PS3Controller->setLedOn(LED1);
+    isPS3ControllerInitialized = true;
+    badPS3Data = 0;
+
+    mainControllerConnected = true;
+    WaitingforReconnect = true;
+
+    #ifdef SHADOW_DEBUG
+       strcat(output, "\r\nWe have the controller connected.\r\n");
+    #endif
+}
+
+// =======================================================================================
+//           Determine if we are having connection problems with the PS3 Controller
+// =======================================================================================
+boolean criticalFaultDetect()
+{
+    if (PS3Controller->PS3Connected)
+    {
+        
+        currentTime = millis();
+        lastMsgTime = PS3Controller->getLastMessageTime();
+        msgLagTime = currentTime - lastMsgTime;            
+        
+        if (WaitingforReconnect)
+        {
+            
+            if (msgLagTime < 200)
+            {
+             
+                WaitingforReconnect = false; 
+            
+            }
+            
+            lastMsgTime = currentTime;
+            
+        } 
+        
+        if ( currentTime >= lastMsgTime)
+        {
+              msgLagTime = currentTime - lastMsgTime;
+              
+        } else
+        {
+
+             msgLagTime = 0;
+        }
+        
+        if (msgLagTime > 300 && !isFootMotorStopped)
+        {
+            #ifdef SHADOW_DEBUG
+              strcat(output, "It has been 300ms since we heard from the PS3 Controller\r\n");
+              strcat(output, "Shut down motors and watching for a new PS3 message\r\n");
+            #endif
+            
+//          You would stop all motors here
+            isFootMotorStopped = true;
+        }
+        
+        if ( msgLagTime > 10000 )
+        {
+            #ifdef SHADOW_DEBUG
+              strcat(output, "It has been 10s since we heard from Controller\r\n");
+              strcat(output, "\r\nDisconnecting the controller.\r\n");
+            #endif
+            
+//          You would stop all motors here
+            isFootMotorStopped = true;
+            
+            PS3Controller->disconnect();
+            WaitingforReconnect = true;
+            return true;
+        }
+
+        //Check PS3 Signal Data
+        if(!PS3Controller->getStatus(Plugged) && !PS3Controller->getStatus(Unplugged))
+        {
+            //We don't have good data from the controller.
+            //Wait 15ms - try again
+            delay(15);
+            Usb.Task();   
+            lastMsgTime = PS3Controller->getLastMessageTime();
+            
+            if(!PS3Controller->getStatus(Plugged) && !PS3Controller->getStatus(Unplugged))
+            {
+                badPS3Data++;
+                #ifdef SHADOW_DEBUG
+                    strcat(output, "\r\n**Invalid data from PS3 Controller. - Resetting Data**\r\n");
+                #endif
+                return true;
+            }
+        }
+        else if (badPS3Data > 0)
+        {
+
+            badPS3Data = 0;
+        }
+        
+        if ( badPS3Data > 10 )
+        {
+            #ifdef SHADOW_DEBUG
+                strcat(output, "Too much bad data coming from the PS3 Controller\r\n");
+                strcat(output, "Disconnecting the controller and stop motors.\r\n");
+            #endif
+            
+//          You would stop all motors here
+            isFootMotorStopped = true;
+            
+            PS3Controller->disconnect();
+            WaitingforReconnect = true;
+            return true;
+        }
+    }
+    else if (!isFootMotorStopped)
+    {
+        #ifdef SHADOW_DEBUG      
+            strcat(output, "No PS3 controller was found\r\n");
+            strcat(output, "Shuting down motors and watching for a new PS3 message\r\n");
+        #endif
+        
+//      You would stop all motors here
+        isFootMotorStopped = true;
+        
+        WaitingforReconnect = true;
+        return true;
+    }
+    
+    return false;
+}
+
+// =======================================================================================
+//           USB Read Function - Supports Main Program Loop
+// =======================================================================================
+boolean readUSB()
+{
+  
+     Usb.Task();
+     
+    //The more devices we have connected to the USB or BlueTooth, the more often Usb.Task need to be called to eliminate latency.
+    if (PS3Controller->PS3Connected) 
+    {
+        if (criticalFaultDetect())
+        {
+            //We have a fault condition that we want to ensure that we do NOT process any controller data!!!
+            printOutput(output);
+            return false;
+        }
+        
+    } else if (!isFootMotorStopped)
+    {
+        #ifdef SHADOW_DEBUG      
+            strcat(output, "No controller was found\r\n");
+            strcat(output, "Shuting down motors, and watching for a new PS3 foot message\r\n");
+        #endif
+        
+//      You would stop all motors here
+        isFootMotorStopped = true;
+        
+        WaitingforReconnect = true;
+    }
+    
+    return true;
+}
+
+// =======================================================================================
+//          Print Output Function
+// =======================================================================================
+
+void printOutput(const char *value)
+{
+    if ((strcmp(value, "") != 0))
+    {
+        if (Serial) Serial.println(value);
+        strcpy(output, ""); // Reset output string
+    }
+}
